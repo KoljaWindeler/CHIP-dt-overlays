4cc72dbac55080fad5f59d111fcf458edd8a67a8
Kolja Windeler <Kolja.Windeler@gmail.com>
2016-11-25 13:45:51 +0000

    Add module to auotload if not done

--- database/update.sh
+++ database/update.sh
@@ -2,3 +2,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/lib / -R
 echo "updated DTO files"
+if grep -Fxq "w1_ds2431" /etc/modules
+then
+	echo "w1_ds4231 already in autoload, skipping"
+else
+	echo "w1_ds2431" >> /etc/modules
+	echo "ws_d2431 added to autoload list"
+fi
+echo "Installation done, please reboot now"
